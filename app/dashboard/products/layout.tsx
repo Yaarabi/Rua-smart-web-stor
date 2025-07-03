c6cb03d5f5fb2b468f1e9d0541474e82818e34b1
Yaarabi <yaarabi318@gmail.com>
2025-07-03 18:30:30 +0100

    fix: update dashboard/products layout

--- app/dashboard/products/layout.tsx
+++ app/dashboard/products/layout.tsx
@@ -1,57 +1,16 @@
 
-'use client';
-import { createContext, useState, ReactNode } from 'react';
+
+import { ReactNode } from 'react';
 
 
-interface Data {
-  id: string;
-  name: string;
-  title: string;
-  description: string;
-  price: number;
-  category: string;
-  stock: number;
-  images: string;
-}
 
-
-interface ProductContextType {
-  productData: Data;
-  setData: React.Dispatch<React.SetStateAction<Data>>;
-}
-
-
-export const ProductContext = createContext<ProductContextType>({
-  productData: {
-    id: '',
-    name: '',
-    title: '',
-    description: '',
-    price: 0,
-    category: '',
-    stock: 0,
-    images: '',
-  },
-  setData: () => {},
-});
 
 
 export default function ProductProvider({ children }: { children: ReactNode }) {
-  const [productData, setData] = useState<Data>({
-    id: '',
-    name: '',
-    title: '',
-    description: '',
-    price: 0,
-    category: '',
-    stock: 0,
-    images: '',
-  });
+  
 
   return (
-    <ProductContext.Provider value={{ productData, setData }}>
       <section>{children}</section>
-    </ProductContext.Provider>
   );
 }
 
