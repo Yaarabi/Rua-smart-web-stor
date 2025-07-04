e198abaec35353e3112a80670f1acfbb0cfbd6e1
Yaarabi <yaarabi318@gmail.com>
2025-07-05 00:41:35 +0100

    fix: change openAI key name

--- app/utils/openai.ts
+++ app/utils/openai.ts
@@ -7,7 +7,7 @@ dotenv.config()
 
 const openai = new OpenAI({
     baseURL: "https://openrouter.ai/api/v1",
-    apiKey: process.env.API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const generatePrompts = async (prompt: string) => {
