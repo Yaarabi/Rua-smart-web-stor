b6d2980313c7b9eff5aefadb111f99348b78cda0
Yaarabi <yaarabi318@gmail.com>
2025-07-05 16:04:45 +0100

    fix: fetch data in built runing...

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,28 @@
 
-// import Header from "@/components/home/header";
-// import Hero from "@/components/home/hero";
-// import Categories from "@/components/home/categories";
-// import About from "@/components/home/about";
-// import Benefits from "@/components/home/benifits";
-// import Newsletter from "@/components/home/trust";
-// import Footer from "@/components/home/footer";
-// import Collection from "@/components/home/collection";
-// export const dynamic = "force-dynamic";
-
-// export default function Home() {
-//   return (
-//     <>
-//       <Header />
-//       <main className="bg-gray-100 text-gray-900 min-h-screen">
-//         <Hero />
-//         <Categories />
-//         <Collection />
-//         <About />
-//         <Benefits />
-//         <Newsletter />
-//       </main>
-//       <Footer />
-//     </>
-//   );
-// }
+import Header from "@/components/home/header";
+import Hero from "@/components/home/hero";
+import Categories from "@/components/home/categories";
+import About from "@/components/home/about";
+import Benefits from "@/components/home/benifits";
+import Newsletter from "@/components/home/trust";
+import Footer from "@/components/home/footer";
+import Collection from "@/components/home/collection";
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <main style={{ padding: "4rem", textAlign: "center" }}>
-      <h1>Rua Web Store is working 🎉</h1>
-    </main>
+    <>
+      <Header />
+      <main className="bg-gray-100 text-gray-900 min-h-screen">
+        <Hero />
+        <Categories />
+        <Collection />
+        <About />
+        <Benefits />
+        <Newsletter />
+      </main>
+      <Footer />
+    </>
   );
 }
 
