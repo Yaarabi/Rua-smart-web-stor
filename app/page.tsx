69034522e6b3028daa8eb9cad4fbe45c6dccdc8a
Yaarabi <yaarabi318@gmail.com>
2025-07-05 15:59:18 +0100

    try to fix an issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,36 @@
 
-import Header from "@/components/home/header";
-import Hero from "@/components/home/hero";
-import Categories from "@/components/home/categories";
-import About from "@/components/home/about";
-import Benefits from "@/components/home/benifits";
-import Newsletter from "@/components/home/trust";
-import Footer from "@/components/home/footer";
-import Collection from "@/components/home/collection";
-export const dynamic = "force-dynamic";
+// import Header from "@/components/home/header";
+// import Hero from "@/components/home/hero";
+// import Categories from "@/components/home/categories";
+// import About from "@/components/home/about";
+// import Benefits from "@/components/home/benifits";
+// import Newsletter from "@/components/home/trust";
+// import Footer from "@/components/home/footer";
+// import Collection from "@/components/home/collection";
+// export const dynamic = "force-dynamic";
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Header />
+//       <main className="bg-gray-100 text-gray-900 min-h-screen">
+//         <Hero />
+//         <Categories />
+//         <Collection />
+//         <About />
+//         <Benefits />
+//         <Newsletter />
+//       </main>
+//       <Footer />
+//     </>
+//   );
+// }
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main className="bg-gray-100 text-gray-900 min-h-screen">
-        <Hero />
-        <Categories />
-        <Collection />
-        <About />
-        <Benefits />
-        <Newsletter />
-      </main>
-      <Footer />
-    </>
+    <main style={{ padding: "4rem", textAlign: "center" }}>
+      <h1>Rua Web Store is working 🎉</h1>
+    </main>
   );
 }
 
