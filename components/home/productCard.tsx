92542f4a78b7a8eee2fc2954978276b536a2e3b2
Yaarabi <yaarabi318@gmail.com>
2025-07-14 01:05:12 +0100

    fix: envirenment variables

--- components/home/productCard.tsx
+++ components/home/productCard.tsx
@@ -1,59 +1,59 @@
 
-import Image from "next/image";
-import React from "react";
-import Link from "next/link";
+// import Image from "next/image";
+// import React from "react";
+// import Link from "next/link";
 
-const ProductItem = ({
-    product,
-    color,
-}: {
-    product: Product;
-    color: string;
-}) => {
-    return (
-        <div className="flex flex-col items-center gap-y-2">
-        <Link href={`/product/${product.slug}`}>
-            <Image
-            src={
-                product.mainImage
-                ? `/${product.mainImage}`
-                : "/product_placeholder.jpg"
-            }
-            width="0"
-            height="0"
-            sizes="100vw"
-            className="w-auto h-[300px]"
-            alt={product?.title}
-            />
-        </Link>
-        <Link
-            href={`/product/${product.slug}`}
-            className={
-            color === "black"
-                ? `text-xl text-black font-normal mt-2 uppercase`
-                : `text-xl text-white font-normal mt-2 uppercase`
-            }
-        >
-            {product.title}
-        </Link>
-        <p
-            className={
-            color === "black"
-                ? "text-lg text-black font-semibold"
-                : "text-lg text-white font-semibold"
-            }
-        >
-            ${product.price}
-        </p>
+// const ProductItem = ({
+//     product,
+//     color,
+// }: {
+//     product: Product;
+//     color: string;
+// }) => {
+//     return (
+//         <div className="flex flex-col items-center gap-y-2">
+//         <Link href={`/product/${product.slug}`}>
+//             <Image
+//             src={
+//                 product.mainImage
+//                 ? `/${product.mainImage}`
+//                 : "/product_placeholder.jpg"
+//             }
+//             width="0"
+//             height="0"
+//             sizes="100vw"
+//             className="w-auto h-[300px]"
+//             alt={product?.title}
+//             />
+//         </Link>
+//         <Link
+//             href={`/product/${product.slug}`}
+//             className={
+//             color === "black"
+//                 ? `text-xl text-black font-normal mt-2 uppercase`
+//                 : `text-xl text-white font-normal mt-2 uppercase`
+//             }
+//         >
+//             {product.title}
+//         </Link>
+//         <p
+//             className={
+//             color === "black"
+//                 ? "text-lg text-black font-semibold"
+//                 : "text-lg text-white font-semibold"
+//             }
+//         >
+//             ${product.price}
+//         </p>
 
-        <Link
-            href={`/product/${product?.slug}`}
-            className="block flex justify-center items-center w-full uppercase bg-white px-0 py-2 text-base border border-black border-gray-300 font-bold text-blue-600 shadow-sm hover:bg-black hover:bg-gray-100 focus:outline-none focus:ring-2"
-        >
-            <p>View product</p>
-        </Link>
-        </div>
-    );
-    };
+//         <Link
+//             href={`/product/${product?.slug}`}
+//             className="block flex justify-center items-center w-full uppercase bg-white px-0 py-2 text-base border border-black border-gray-300 font-bold text-blue-600 shadow-sm hover:bg-black hover:bg-gray-100 focus:outline-none focus:ring-2"
+//         >
+//             <p>View product</p>
+//         </Link>
+//         </div>
+//     );
+//     };
 
-export default ProductItem;
+// export default ProductItem;
