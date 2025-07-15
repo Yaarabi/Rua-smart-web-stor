ae12acea5af1b966d6a84f2907865d46d8800359
Yaarabi <yaarabi318@gmail.com>
2025-07-16 00:49:34 +0100

    recover GA4, with alternative way

--- app/api/trafic/activity/route.ts
+++ app/api/trafic/activity/route.ts
@@ -1,44 +1,39 @@
 
-// import { NextResponse } from 'next/server';
-// import { BetaAnalyticsDataClient } from '@google-analytics/data';
+import { NextResponse } from 'next/server';
+import { BetaAnalyticsDataClient } from '@google-analytics/data';
 
-// const credentials =
-//     process.env.GA_SERVICE_ACCOUNT
-//         ? JSON.parse(Buffer.from(process.env.GA_SERVICE_ACCOUNT, 'base64').toString('utf-8'))
-//         : undefined;
+const credentials =
+    process.env.GA_SERVICE_ACCOUNT
+        ? JSON.parse(Buffer.from(process.env.GA_SERVICE_ACCOUNT, 'base64').toString('utf-8'))
+        : undefined;
 
-// const analyticsClient = new BetaAnalyticsDataClient({
-//     credentials,
-// });
+const analyticsClient = new BetaAnalyticsDataClient({
+    credentials,
+});
 
-// export async function GET() {
-//     // 
-//     try {
-//         if (!credentials) {
-//         throw new Error('GA_SERVICE_ACCOUNT credentials not found or invalid.');
-//         }
-
-//         const [activityResponse] = await analyticsClient.runReport({
-//         property: process.env.PROPERTIES_GA4,
-//         dateRanges: [{ startDate: '7daysAgo', endDate: 'today' }],
-//         dimensions: [{ name: 'country' }, { name: 'date' }],
-//         metrics: [
-//             { name: 'activeUsers' },
-//             { name: 'userEngagementDuration' },
-//         ],
-//         limit: 100,
-//         });
-
-//         return NextResponse.json({
-//         activityUsersData: activityResponse.rows,
-//         });
-//     }  catch (error) {
-//         console.error('Cart Data Error:', error);
-//         return NextResponse.json({ message: 'Failed to fetch cart data', error }, { status: 500 });
-//     }
-// }
 export async function GET() {
-  const raw = process.env.GA_SERVICE_ACCOUNT;
-  const decoded = raw ? Buffer.from(raw, 'base64').toString('utf-8') : 'Missing or invalid';
-  return new Response(decoded);
+    // 
+    try {
+        if (!credentials) {
+        throw new Error('GA_SERVICE_ACCOUNT credentials not found or invalid.');
+        }
+
+        const [activityResponse] = await analyticsClient.runReport({
+        property: process.env.PROPERTIES_GA4,
+        dateRanges: [{ startDate: '7daysAgo', endDate: 'today' }],
+        dimensions: [{ name: 'country' }, { name: 'date' }],
+        metrics: [
+            { name: 'activeUsers' },
+            { name: 'userEngagementDuration' },
+        ],
+        limit: 100,
+        });
+
+        return NextResponse.json({
+        activityUsersData: activityResponse.rows,
+        });
+    }  catch (error) {
+        console.error('Cart Data Error:', error);
+        return NextResponse.json({ message: 'Failed to fetch cart data', error }, { status: 500 });
+    }
 }
